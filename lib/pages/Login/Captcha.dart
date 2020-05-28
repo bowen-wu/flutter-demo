2e1a41b03950dfdf9c8ec49ac6d7bbe39d202960
wubowen <wubowen@treefintech.com>
2020-05-28 17:30:34 +0800

    feat: complete captcha input

--- lib/pages/Login/Captcha.dart
+++ lib/pages/Login/Captcha.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quick_loan_demo/model/loginModel.dart';
 
 class Captcha extends StatefulWidget {
   Captcha({Key key, this.title}) : super(key: key);
@@ -10,19 +12,50 @@ class Captcha extends StatefulWidget {
 }
 
 class _Captcha extends State<Captcha> {
+  String _captcha = '';
+
+  TextEditingController _captchaInputController = TextEditingController();
+  FocusNode _captchaFocus = FocusNode();
+  FocusScopeNode focusScopeNode;
+  final int _captchaNumber = 6;
+
+  List<Widget> _ConstructorSquare() {
+    List<Widget> squareList = [];
+    for(var i = 0; i < _captchaNumber; i++) {
+      squareList.add(
+        Padding(
+          padding: EdgeInsets.only(right: i == 5 ? 0 : 9),
+          child: Container(
+            width: 48,
+            height: 48,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              border: Border.all(width: 1, color: _captcha.length == i || (i == 5 && _captcha.length >= 51) ? Color.fromRGBO(255, 96, 81, 1) : Color.fromRGBO(231, 231, 231, 1)),
+              borderRadius: BorderRadius.all(Radius.circular(2)),
+            ),
+            child: Center(
+              child: Text(
+                '${i + 1 <= _captcha.length ? _captcha.substring(i, i + 1) : ''}',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(51, 51, 51, 1),
+                ),
+              ),
+            ),
+          ),
+        )
+      );
+    }
+    return squareList;
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('Basic AppBar'),
         backgroundColor: Color(0xFFFFFFFF),
         elevation: 0,
@@ -37,7 +70,103 @@ class _Captcha extends State<Captcha> {
         color: Color.fromRGBO(255, 255, 255, 1),
         child: Padding(
           padding: EdgeInsets.only(left: 16, right: 16),
-          child: Text('验证码！')
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '请输入验证码',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color.fromRGBO(51, 51, 51, 1),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8, bottom: 38),
+                child: Text(
+                  '验证码已通过短信发送至 ${Provider.of<LoginModel>(context, listen: false).phoneNumber}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color.fromRGBO(51, 51, 51, 1),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 11),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        '${_captchaNumber}位数字验证码',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color.fromRGBO(153, 153, 153, 1),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 0,
+                      child: Listener(
+                        onPointerDown: (event) {
+                          print(1);
+                        },
+                        child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1, color: Color.fromRGBO(153, 153, 153, 1)),
+                              borderRadius: BorderRadius.all(Radius.circular(11)),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
+                              child: Text(
+                                '59s后重新获取',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Color.fromRGBO(153, 153, 153, 1),
+                                ),
+                              ),
+                            )
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Offstage(
+                offstage: true,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  cursorWidth: 0,
+                  cursorColor: Color.fromRGBO(255, 255, 255, 1),
+                  autofocus: false,
+                  focusNode: _captchaFocus,
+                  controller: _captchaInputController,
+                  maxLength: _captchaNumber,
+                  onChanged: (event) {
+                    setState(() {
+                      _captcha = event;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    counterText: '',
+                    hintText: '',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Listener(
+                onPointerDown: (event) {
+                  if(null == focusScopeNode){
+                    focusScopeNode = FocusScope.of(context);
+                  }
+                  focusScopeNode.requestFocus(_captchaFocus);
+                },
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: _ConstructorSquare(),
+                ),
+              )
+            ],
+          )
         ),
       ),
     );
